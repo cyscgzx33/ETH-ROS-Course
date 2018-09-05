4ed745eccf7d50087df7978034abc42ca911ef85
Ethan_C <chenyus@umich.edu>
2018-09-05 16:48:09 -0400

    fixed some errors about the subscriber

--- husky_highlevel_controller_new/src/husky_highlevel_controller_new_node.cpp
+++ husky_highlevel_controller_new/src/husky_highlevel_controller_new_node.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
 
 	// Exercise 2-5: Create a subscriber to the /scan topic.
 	// Exercise 2-6: create a callback method to record the minimum distance of scan range
-	ros::Subscriber scan_subscriber_ = nodeHandle.subscribe("scan", 1, &husky_highlevel_controller_new::HuskyHighlevelControllerNew::scanCallback, &huskyHighlevelControllerNew);
+	ros::Subscriber scan_subscriber_ = nodeHandle.subscribe("scan", queue_size, &husky_highlevel_controller_new::HuskyHighlevelControllerNew::scanCallback, &huskyHighlevelControllerNew);
 
 	// ros::spin() is a common way of callbacks
 	ros::spin();
